3ce5c3c563ba15abfe16faa696c947798ebbf050
TheQuyen <quyen.th.bui@gmail.com>
2018-06-03 11:01:07 +0700

    'bum'

--- Rocket/src/PlayerShoot.java
+++ Rocket/src/PlayerShoot.java
@@ -3,26 +3,25 @@ import java.util.List;
 
 public class PlayerShoot {
     public List<Bullet> bulletPlayers;
-    private int count = 0;
+    private FrameCounter frameCounter;
 
     public PlayerShoot() {
+        this.frameCounter = new FrameCounter(40);
         this.bulletPlayers = new ArrayList<>();
     }
 
     public void run(Player player) {
-        if (this.count == 40) {
+        if (this.frameCounter.run()) {
             Bullet bulletPlayer = new Bullet();
             bulletPlayer.position.set(player.position);
 
             Vector2D rotate = player.playerMove.velocity.add(
-                    (new Vector2D(2,0)).rotate(player.playerMove.angle)
+                    (new Vector2D(2, 0)).rotate(player.playerMove.angle)
             );
 
             bulletPlayer.velocity.set(rotate);
             this.bulletPlayers.add(bulletPlayer);
-            this.count = 0;
-        } else {
-            this.count += 1;
+            this.frameCounter.reset();
         }
     }
 }
--- Rocket/src/PolygonRenderer.java
+++ Rocket/src/PolygonRenderer.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class PolygonRenderer implements Renderer {
 
-    private List<Vector2D> verties;
+    private List<Vector2D> vertices;
     private Polygon polygon;
     public double angle = 0.0;
     private Color color;
 
     public PolygonRenderer(Color color, Vector2D... verties) {
-        this.verties = Arrays.asList(verties);
+        this.vertices = Arrays.asList(verties);
         this.color = color;
         this.polygon = new Polygon();
     }
@@ -25,15 +25,15 @@ public class PolygonRenderer implements Renderer {
     public void updatePolygon(Vector2D position) {
         this.polygon.reset();
 
-        Vector2D center = this.verties
+        Vector2D center = this.vertices
                 .stream()
                 .reduce(new Vector2D(), (v1, v2) -> v1.add(v2))
-                .multiply(1.0f / this.verties.size())
+                .multiply(1.0f / this.vertices.size())
                 .rotate(this.angle);
 
         Vector2D translate = position.subtract(center);
 
-        this.verties.stream()
+        this.vertices.stream()
                 .map(vector2D -> vector2D.rotate(angle))
                 .map(vector2D -> vector2D.add(translate))
                 .forEach(vertex -> polygon.addPoint((int)vertex.x, (int)vertex.y));
