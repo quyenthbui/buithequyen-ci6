81422702a82c1f36ab55726088afedf5a8d717c5
TheQuyen <quyen.th.bui@gmail.com>
2018-06-03 10:56:41 +0700

    'tiep'

--- Rocket/src/EnemyShoot.java
+++ Rocket/src/EnemyShoot.java
@@ -3,25 +3,22 @@ import java.util.List;
 
 public class EnemyShoot {
     public List<Bullet> bulletEnemies;
-
-    private int count = 0;
+    private FrameCounter frameCounter;
 
     public EnemyShoot() {
+        this.frameCounter = new FrameCounter(50);
         this.bulletEnemies = new ArrayList<>();
     }
 
     public void run(Enemy enemy) {
-        // create bullet
-        if (this.count == 50) {
-            for (double angle =0.0; angle <= 360.0; angle += 15.0){
+        if (this.frameCounter.run()) {
+            for (double angle = 0.0; angle <= 360.0; angle += 15.0) {
                 Bullet bulletEnemy = new Bullet();
                 bulletEnemy.position.set(enemy.position);
-                bulletEnemy.velocity.set((new Vector2D(3,0)).rotate(angle));
+                bulletEnemy.velocity.set((new Vector2D(3, 0)).rotate(angle));
                 this.bulletEnemies.add(bulletEnemy);
             }
-            this.count = 0;
-        } else {
-            this.count += 1;
+            this.frameCounter.reset();
         }
     }
 }
